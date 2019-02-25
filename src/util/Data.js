dba2ced7ebbf82d67aaa042d4235d525fa3e7512
Pete Campbell <petecampbell@petes-mbp.home>
2019-02-25 00:04:42 +0000

    Testing viability of using a class to create the data for each project

---
+++ src/util/Data.js
@@ -0,0 +1,22 @@
+class Data {
+    constructor(id, name, type, desc) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.desc = desc;
+
+        console.log(this.name + ' was created');
+    }
+}
+
+let harronfr = new Data(1, "Harron Framing", ["Website Design", "Advertisement Design"], "This is a description of a project");
+
+let array = [];
+for(i = 0; i < 6; i++) {
+    let id = i + 1;
+    let temp = new Data(id, "Test: " + id, ["type1", "type2"], "A test description for project " + id);
+    array.push(temp);
+}
+
+console.log(harronfr.id + ': ' + harronfr.name);
+console.log(array[3]);
