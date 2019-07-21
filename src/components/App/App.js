e7dd914cbe35a2f22bc3c34e7644704e9889b2a0
Pete Campbell <pete.campbell321@gmail.com>
2019-07-21 13:44:25 +0100

    Fixed routing on Choice component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,14 @@
+// Import Statements
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+// Pages
 import Home from '../../pages/Home/Home';
 import Admin from '../../pages/Admin/Admin';
 import Projects from '../../pages/Projects/Projects';
 import About from '../../pages/About/About';
 
+// CSS
 import './App.css';
 
 class App extends Component {
--- src/components/Choice/Choice.js
+++ src/components/Choice/Choice.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 // CSS
 import "../universal.css";
@@ -10,43 +10,32 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { faFolderOpen } from '@fortawesome/free-solid-svg-icons';
 
-// Links
-import App from '../App/App';
-
 class Choice extends Component {
     render() {
         return(
-            <Router>
-                <Switch>
-                    <Route path="/projects" component={App} />
-                    <Route path="/about" component={App} />
-
-                    <div className="choice-container" id="choice">
-                        <h2 className="section-title">
-                            Choose Your Path...
-                        </h2>
-                        <div className="option-container">
-                                <div className="option" id="projects">
-                                    <Link to="/projects">
-                                        <FontAwesomeIcon icon={faFolderOpen} size="3x" />
-                                        <h3>
-                                            Projects
-                                        </h3>
-                                    </Link>
-                                </div>
-                                <div className="option" id="about">
-                                    <Link to="/about">
-                                        <FontAwesomeIcon icon={faUser} size="3x" />
-                                        <h3>
-                                            About Me
-                                        </h3>
-                                    </Link>
-                                </div>
+            <div className="choice-container" id="choice">
+                <h2 className="section-title">
+                    Choose Your Path...
+                </h2>
+                <div className="option-container">
+                        <div className="option" id="projects">
+                            <Link to="/projects">
+                                <FontAwesomeIcon icon={faFolderOpen} size="3x" />
+                                <h3>
+                                    Projects
+                                </h3>
+                            </Link>
                         </div>
-                    </div>
-
-                </Switch>
-            </Router>
+                        <div className="option" id="about">
+                            <Link to="/about">
+                                <FontAwesomeIcon icon={faUser} size="3x" />
+                                <h3>
+                                    About Me
+                                </h3>
+                            </Link>
+                        </div>
+                </div>
+            </div>
         );
     }
 }
