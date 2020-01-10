4f982a21854eadc033344a55ffc1b2d6d447a20c
Peter Campbell <pcampbell52@qub.ac.uk>
2020-01-10 11:59:43 +0000

    Build files for publishing on Netlify

---
+++ build/precache-manifest.8c9188173e6a9175907003f796cc7ff5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f74e02554ea9f578c4e38a97ff0b0736",
+    "url": "/portfolio-react/static/media/profile.f74e0255.JPG"
+  },
+  {
+    "revision": "780bcbc715c88212df47",
+    "url": "/portfolio-react/static/js/runtime~main.780bcbc7.js"
+  },
+  {
+    "revision": "e97845974276d859af52",
+    "url": "/portfolio-react/static/js/main.e9784597.chunk.js"
+  },
+  {
+    "revision": "2a636fb1b5b3d639c9c2",
+    "url": "/portfolio-react/static/js/2.2a636fb1.chunk.js"
+  },
+  {
+    "revision": "e97845974276d859af52",
+    "url": "/portfolio-react/static/css/main.17352819.chunk.css"
+  },
+  {
+    "revision": "833a2832c56e4305246d75fab6d94b26",
+    "url": "/portfolio-react/index.html"
+  }
+];
